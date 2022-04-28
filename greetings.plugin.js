20a8b147e8417e72ae8875656c5908f14cf1bf5c
ale933 <73939749+ale933@users.noreply.github.com>
2022-04-28 19:54:16 +0200

    Add files via upload

---
+++ greetings.plugin.js
@@ -0,0 +1,26 @@
+/**
+ * @name Greetings
+ * @author AleSira
+ * @version 1.0.0
+ * @description Greetings for each log
+ * @website https://alesira.pythonanywhere.com/
+ * @authorLink https://www.instagram.com/aleiracusa9026/
+ */
+
+ 
+
+module.exports = class name{
+    load() { }
+    start() {
+        const shitposts = ["Ciao!", "Bella Bro!", "Bentornato, ciccione obeso", "Welcome Back!", "Bentornato bellezza <3", "Wassup", "Chiudi Discord e fatti una vita 😡"];
+
+        const randomShitposts = shitposts[Math.floor(Math.random() * shitposts.length)];
+        BdApi.showToast(randomShitposts, {type: "success", icon: false, timeout: 7000})
+        
+    }
+
+    stop() {
+        BdApi.alert("Plugin disactivated", "See you next time!")
+
+    }
+}
